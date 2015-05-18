59f2c3d10f3b949122431c70dca6c28fc0f06b9f
Fabian Becker <halfdan@xnorfz.de>
2015-05-18 20:06:51 +0200

    Add local buffer and simplistic test suite for tracking events

--- test/tracker_spec.js
+++ test/tracker_spec.js
@@ -1,25 +1,72 @@
 var sinon   = require('sinon'),
     should  = require('should'),
+    uuid    = require('node-uuid'),
     Tracker = require('../lib/tracker');
 
 /*global describe, it, before, beforeEach, after, afterEach */
 describe('Tracker', function () {
 
-    describe('#track', function () {
-        var tracker;
+  describe('#track', function () {
+    var tracker;
 
-        beforeEach(function () {
-            tracker = new Tracker();
-        });
-
-        it('should emit event when called', function () {
-            var spy = sinon.spy();
-
-            tracker.on('track.new', spy);
-            tracker.track();
-
-            spy.called.should.be.true;
-        });
+    beforeEach(function () {
+      tracker = new Tracker();
     });
+
+    it('emits event when called', function () {
+      var spy = sinon.spy();
+
+      tracker.on('post.visit', spy);
+      tracker.track(
+          uuid.v4(),
+          'post',
+          'visit'
+      );
+
+      spy.called.should.be.true;
+    });
+
+    it('emits uuid/options when called', function (done) {
+      var uid = uuid.v4();
+
+      tracker.on('post.visit', function (data) {
+        data.uuid.should.equal(uid);
+        done();
+      });
+
+      tracker.track(
+          uid,
+          'post',
+          'visit'
+      );
+    });
+
+    it('emits events for all subevents', function () {
+      var spy = sinon.spy();
+
+      tracker.on('post.visit', spy);
+      tracker.on('post.visit.social', spy);
+      tracker.track(
+          uuid.v4(),
+          'post',
+          'visit.social'
+      );
+
+      spy.calledTwice.should.be.true;
+    });
+
+    it('errors if arguments are missing', function () {
+      var missingResourceType = function () {
+            tracker.track(uuid.v4());
+          },
+          missingEventName = function () {
+            tracker.track(uuid.v4(), 'post');
+          };
+
+      // Missing resource_type and event_name
+      missingResourceType.should.throwError('Missing arguments');
+      missingEventName.should.throwError('Missing arguments');
+    });
+  });
 });
 
